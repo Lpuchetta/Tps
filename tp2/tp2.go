6b23d4ca015bc29472f6fe03b4a35be8c7d1b017
Lorenzo Puchetta <lpuchetta@fi.uba.ar>
2025-05-31 11:56:34 -0300

    Modifque firma

--- tp2/tp2.go
+++ tp2/tp2.go
@@ -1,7 +1,7 @@
 package tp2
 type Aeropuerto interface{
 	/*Agregar_archivo recbie el nombre d eun archivo .csv y lo procesa*/ 
-	Agregar_Archivo(nombreArchivo string)
+	Agregar_Archivo(nombreArchivo string) error
 	
 	Ver_Tablero(K int, modo, desde, hasta string )
 

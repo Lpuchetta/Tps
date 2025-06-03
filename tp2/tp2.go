c1d52c9962c6adac97d7527142d6b44937c2a211
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-06-03 19:42:31 -0300

    Cambio sin importancia - Ignorar

--- tp2/tp2.go
+++ tp2/tp2.go
@@ -1,10 +1,10 @@
 package tp2
 
-type Aeropuerto interface{
-	/*Agregar_archivo recbie el nombre d eun archivo .csv y lo procesa*/ 
+type Aeropuerto interface {
+	/*Agregar_archivo recbie el nombre d eun archivo .csv y lo procesa*/
 	Agregar_Archivo(nombreArchivo string)
-	
-	Ver_Tablero(K int, modo, desde, hasta string )
+
+	Ver_Tablero(K int, modo, desde, hasta string)
 
 	Info_Vuelo(codigo string)
 

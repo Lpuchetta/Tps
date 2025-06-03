d918b68547ac93d8411e0b2674d9baca896dbeee
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-06-03 19:39:51 -0300

    Agrego correción a la primitiva MostrarInfo de vuelo - cambio deBoolAInt por boolComoString

--- tp2/vuelo/imple_vuelos.go
+++ tp2/vuelo/imple_vuelos.go
@@ -1,51 +1,52 @@
-	package vuelo
-import(
-	"time"
+package vuelo
+
+import (
 	"fmt"
+	"time"
 )
 
-type vuelo struct{
-	codigo        string
-	aerolinea     string
-	origen        string
-	destino       string
-	matricula     string
-	prioridad     int
-	fecha         time.Time
-	retraso       int
-	tiempoVuelo   int
-	cancelado     bool
+type vuelo struct {
+	codigo      string
+	aerolinea   string
+	origen      string
+	destino     string
+	matricula   string
+	prioridad   int
+	fecha       time.Time
+	retraso     int
+	tiempoVuelo int
+	cancelado   bool
 }
 
-func CrearVuelo(codigo, aerolinea, origen, destino, matricula string, prioridad int, fecha time.Time, retraso, tiempo int, cancelado bool) Vuelo{
+func CrearVuelo(codigo, aerolinea, origen, destino, matricula string, prioridad int, fecha time.Time, retraso, tiempo int, cancelado bool) Vuelo {
 	return &vuelo{
-		codigo: codigo,
-		aerolinea: aerolinea,
-		origen: origen,
-		destino: destino,
-		matricula: matricula,
-		prioridad: prioridad,
-		fecha: fecha,
-		retraso: retraso,
+		codigo:      codigo,
+		aerolinea:   aerolinea,
+		origen:      origen,
+		destino:     destino,
+		matricula:   matricula,
+		prioridad:   prioridad,
+		fecha:       fecha,
+		retraso:     retraso,
 		tiempoVuelo: tiempo,
-		cancelado: cancelado,
+		cancelado:   cancelado,
 	}
 }
 
-func (v *vuelo) ObtenerFecha() time.Time{
+func (v *vuelo) ObtenerFecha() time.Time {
 	return v.fecha
 }
 
-func (v *vuelo) ObtenerPrioridad() int{
+func (v *vuelo) ObtenerPrioridad() int {
 	return v.prioridad
 }
 
-func (v * vuelo) EstaCancelado() bool{
+func (v *vuelo) EstaCancelado() bool {
 	return v.cancelado
 }
 
-func (v *vuelo) MostrarInfo() string{
-	return fmt.Sprintf("%s %s %s %s %s %d %s %02d %d %d",
+func (v *vuelo) MostrarInfo() string {
+	return fmt.Sprintf("%s %s %s %s %s %d %s %d %d %s",
 		v.codigo,
 		v.aerolinea,
 		v.origen,
@@ -55,42 +56,41 @@ func (v *vuelo) MostrarInfo() string{
 		v.fecha.Format("2006-01-02T15:04:05"),
 		v.retraso,
 		v.tiempoVuelo,
-		deBoolAInt(v.cancelado),
+		boolComoString(v.cancelado),
 	)
 }
 
-func (v *vuelo) ObtenerRetraso() int{
+func (v *vuelo) ObtenerRetraso() int {
 	return v.retraso
 }
 
-func (v *vuelo) ObtenerTiempoVuelo() int{
+func (v *vuelo) ObtenerTiempoVuelo() int {
 	return v.tiempoVuelo
 }
 
-func (v *vuelo) ObtenerMatricula() string{
+func (v *vuelo) ObtenerMatricula() string {
 	return v.matricula
 }
 
-func (v *vuelo) ObtenerOrigen() string{
+func (v *vuelo) ObtenerOrigen() string {
 	return v.origen
 }
 
-func (v *vuelo) ObtenerDestino() string{
+func (v *vuelo) ObtenerDestino() string {
 	return v.destino
 }
 
-func (v *vuelo) ObtenerCodigo() string{
+func (v *vuelo) ObtenerCodigo() string {
 	return v.codigo
 }
 
-func (v *vuelo) ObtenerAerolinea() string{
+func (v *vuelo) ObtenerAerolinea() string {
 	return v.aerolinea
 }
 
-
-func deBoolAInt(b bool) int{
-	if b{
-		return 1
+func boolComoString(b bool) string {
+	if b {
+		return "1"
 	}
-	return 0
+	return "0"
 }

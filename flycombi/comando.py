cc59836f441eab8c7c7aec978a9b9bd54b9f6cb7
Lorenzo Puchetta <lpuchetta@fi.uba.ar>
2025-06-18 22:10:29 -0300

    Agrego una guia

---
+++ flycombi/comando.py
@@ -0,0 +1,4 @@
+def ejecutar_comandos(linea, grafo, aeropuertos):
+    """Aca recibo los comandos ingresados"""
+    """llamo desde este archivo a los archivos que
+    pertencen a biblioteca_grafo"""
